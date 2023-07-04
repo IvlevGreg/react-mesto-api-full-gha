5b67551c98278b94be2a67e8c9e403891f00c3e8
Ivlev Grigoriy <igs94@mail.ru>
2023-07-04 19:33:52 +0300

    feat: add winston logger

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+
 const errorHandler = require('./middlewares/errorHandler');
 const createCustomErrors = require('./middlewares/createCustomErrors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const routes = require('./routes');
 
@@ -14,7 +16,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use(requestLogger);
+
 app.use('/', routes);
+
+app.use(errorLogger)
 
 app.use(errors());
 app.use(createCustomErrors);
