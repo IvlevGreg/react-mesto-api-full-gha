f0c78c8cbaef364cd3ff4ba8d41698e6534d2eab
Ivlev Grigoriy <igs94@mail.ru>
2023-07-12 21:10:02 +0700

    feat: add prod front url to cors white list

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors({ origin: ['http://localhost:3000'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:3000', 'https://blogogram.nomoreparties.sbs'], credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
