7d8d7b74d2d620ceb058ea9c96208ab8fb4d024e
Ivlev Grigoriy <igs94@mail.ru>
2023-07-12 20:39:04 +0700

    refactor: base bac port to 3000

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const routes = require('./routes');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 app.use(cors({ origin: ['http://localhost:3000'], credentials: true }));
 app.use(express.json());
